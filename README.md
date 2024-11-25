228139f5c95a10a3d1527841d050730a5dcaf350
jroper7 <josephroper57@gmail.com>
2024-11-25 01:50:05 -0500

    last commit

--- README.md
+++ README.md
@@ -9,4 +9,9 @@ To run the app you can use the command
 
 npm serve
 
-https://in-info-web4.luddy.indianapolis.iu.edu/~joroper/advWebDev/cookNook/
+
+I had issues with a couple of things, they will be fixed soon.
+
+This application is a website about cooking recipes. You can add, edit, create and delete recipes of your choosing but only after you log in! You can browse at recipes and see the home page while being logged out. But to get the premium stuff you have to create an account/login!
+
+This application uses injection, mvc, dist and src folder structure with webpack as well. The application is run through the dist folder which contains my app.js, assests, css and scss, pages and index. While my js files are run in src. The src folder is where we develop and dynamically change things with javascript. For example, things are added to the page when a user logs in. That is what the src folder is for. The dist folder loads the compiled app.js file which reads the application and injects content onto the page!
--- src/index.js
+++ src/index.js
@@ -11,6 +11,31 @@ const nav = document.querySelector(".nav");
 
 const loginLogoutBtn = $("#login");
 
+
+const userRecipes = [];
+
+function addRecipeListeners(){
+    $("#ingredBtn").on("click", function(){
+
+    })
+    $("#instructBtn").on("click", function(){
+        
+    })
+    $("#subtmitBtn").on("click", function(){
+        
+    })
+}
+
+
+function removeRecipeListeners(){
+    
+    $("#submitBtn").off("click");
+    $("#instructBtn").off("click");
+    $("#ingredBtn").off("click");
+}
+
+
+
 hamburgerMenu.addEventListener("click", () => {
     nav.classList.toggle("active")
     // console.log(nav)
@@ -19,7 +44,7 @@ hamburgerMenu.addEventListener("click", () => {
 
 function changeRoute() {
     let hashTag = window.location.hash;
-    let pageID = hashTag.replace('#', '') || 'yourRecipe'; 
+    let pageID = hashTag.replace('#', '') || 'home'; 
     console.log("Current page ID:", pageID);  // Debug log
 
     
@@ -31,6 +56,7 @@ function changeRoute() {
         console.log("Changing to home background");
         $('.background-wrapper').css('background-image', 'linear-gradient(rgba(242, 92, 84, 0.6), rgba(242, 92, 84, 0.6)), url(../assests/hero.jpg)'); 
         // $('.background-wrapper').css('background-color', 'rgba(242, 92, 84, 0.6)'); 
+        
     } else if (pageID === 'login') {
         console.log("Changing to login background");
         $('.background-wrapper').css({
@@ -38,6 +64,15 @@ function changeRoute() {
             'background-color': '#FFD972',  
             'min-height': '100%'
         });
+        
+    }  else if (pageID === 'browse') {
+        console.log("Changing to browse background");
+        $('.background-wrapper').css('background-image', 'linear-gradient(rgba(167, 232, 189, 0.6), rgba(167, 232, 189, 0.6)), url(../assests/recipe-hero.jpg)');
+
+    }  else if (pageID === 'yourRecipe') {
+        console.log("Changing to browse background");
+        $('.background-wrapper').css('background-image', 'linear-gradient(rgba(167, 232, 189, 0.6), rgba(167, 232, 189, 0.6)), url(../assests/recipe-hero.jpg)');
+
     } else {
         console.log("Changing to default background");
         $('.background-wrapper').css('background-image', 'none');
@@ -79,19 +114,22 @@ function initListeners() {
         signUserUp(firstName, lastName, email, password);
     });
 
+
+    
+
     // Sign-In form logic
-    $(document).ready(function() {
-        $("#login-submit").on("click", (e) => {
-            e.preventDefault(); 
-            console.log("Login button clicked!");  // Log when the login button is clicked
-        
-            const email = $("#login-email").val();
-            const password = $("#login-password").val();
-        
-            console.log("Signing in with:", email, password);
-            signUserIn(email, password);
-        });
+    
+    $("#login-submit").on("click", (e) => {
+        e.preventDefault(); 
+        console.log("Login button clicked!");  // Log when the login button is clicked
+    
+        const email = $("#login-email").val();
+        const password = $("#login-password").val();
+    
+        console.log("Signing in with:", email, password);
+        signUserIn(email, password);
     });
+ 
 
 
     $("#login-submit").on("click", function() {
@@ -103,42 +141,116 @@ function initListeners() {
 
 
     loginLogoutBtn.on("click", () => {
-        const isLoggedIn = auth.currentUser;  // Check if user is logged in
-
+        const isLoggedIn = auth.currentUser;  
+    
         if (isLoggedIn) {
             console.log("Logging out...");
-            logoutUser();  // Log the user out
+            signOut(auth)  // Sign out the user
+                .then(() => {
+                    alert("User logged out");  
+                    console.log("User logged out");
+                    updateLoginLogoutButton();  
+                })
+                .catch((error) => {
+                    console.error("Error logging out: ", error);
+                });
         } else {
             console.log("Redirecting to login...");
-            window.location.hash = "#login";  
+            window.location.hash = "#login"; 
         }
     });
-
+    
 }
 
-const updateLoginLogoutButton = () => {
+
+function recipeListener(){
+    console.log("recipe Listener!!!")
+    $("#ingredBtn").on("click", function () {
+        
+        console.log("Ingredient button clicked");
+        // console.log($("#ingredBtn"))
+        // let currentIngredCount = $(".ingreds input").length;
+        // currentIngredCount++;
+        // $(".ingreds").append(
+        //     `<input type="text" id="ingred${currentIngredCount}" placeholder="ingredient ${currentIngredCount}">`
+        // );
+    });
+    
+    
+    $("#instructBtn").on("click", () => {
+        console.log("Instruction button clicked");
+        let currentInstructCount = $(".instructs input").length;
+        currentInstructCount++;
+        $(".instructs").append(
+            `<input type="text" id="instruct${currentInstructCount}" placeholder="instruction ${currentInstructCount}">`
+        );
+        console.log("Instruction added");
+    });
+    
+    $("#submitBtn").on("click", function () {
+        let recipe = {
+            recipeName: $("#recipeName").val(),
+            recipeImage: $("#imageURL").val(),
+            ingredients: [],
+            instructions: [],
+        };
+    
+        $(".ingreds input").each(function () {
+            recipe.ingredients.push($(this).val());
+        });
+    
+        $(".instructs input").each(function () {
+            recipe.instructions.push($(this).val());
+        });
+    
+        userRecipes.push(recipe);
+        alert("Recipe submitted");
+        $(".form input").val(""); // Reset all input fields
+        console.log(userRecipes);
+    });
+}
+
+
+function updateLoginLogoutButton () {
     const isLoggedIn = auth.currentUser;  // Check if user is logged in
 
     if (isLoggedIn) {
-        loginLogoutBtn.css("Logout");  
+        loginLogoutBtn.html("Logout");  
+      
+        const userRecipesLink = '<li class="nav-link" id="yourRecipe"><a href="#yourRecipe">Your Recipes</a></li>';
+        console.log("anchor clicked")
+       
+        if (!$("#yourRecipe").length) { 
+            $(".nav--ul__one").append(userRecipesLink);
+            $("#yourRecipe").on("click", function (e) {
+                e.preventDefault(); // Prevent default navigation behavior
+                console.log("Navigating to Your Recipes...");  
+                window.location.hash = "#yourRecipe"; // Update the hash
+                changeRoute(); // Trigger the route change manually
+            });
+        }
+
     } else {
-        loginLogoutBtn.css("Login");  
+        loginLogoutBtn.html("Login");
+        // Remove the "Your Recipes" link if user logs out
+        $("#your-recipes-link").remove();
     }
-}
+};
+
 
 auth.onAuthStateChanged(user => {
     updateLoginLogoutButton();  // Update the button text based on auth state
 
     if (user) {
-        
         window.location.hash = "#home";
         loadPage("home"); 
     }
 });
 
 const logoutUser = () => {
-    signOut(auth)  // Use Firebase's signOut to log out the user
+    signOut(auth)  
         .then(() => {
+            alert("user logged out");
             console.log("User logged out");
         })
         .catch((error) => {
@@ -151,6 +263,7 @@ const logoutUser = () => {
 
 
 function initURLListener() {
+   
     
     $(window).on('hashchange', changeRoute);
     
@@ -159,9 +272,11 @@ function initURLListener() {
 
 $(document).ready(function () {
     loadPage("home");
+   
     initListeners(); 
-    
+    recipeListener()
     initURLListener();
+    
     updateLoginLogoutButton();  
 });
 
